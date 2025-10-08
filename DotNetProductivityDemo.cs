6f71c6831ab98195acbf073be5d9e9ef87484b4e
Areeba04 <aribahhussain08@gmail.com>
2025-10-08 21:41:07 +0500

    Add files via upload
    This program demonstrates key features of .NET and C# including productivity, robustness, cross-platform compatibility, and multi-paradigm programming support. 
    It showcases simplified syntax, strong type safety, garbage collection, and concurrent execution using threads — highlighting why .NET is an efficient and modern framework for scalable applications.

---
+++ DotNetProductivityDemo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Calculate(int x, int y)
+    {
+        Console.WriteLine("Sum: " + (x + y));
+    }
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Calculate(a, b); // call method
+    }
+}
