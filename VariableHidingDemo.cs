64b0dad173028e7f60ad9757e5d717b075e75efd
Areeba04 <aribahhussain08@gmail.com>
2025-10-08 21:47:47 +0500

    Add files via upload
    This program demonstrates the concept of inheritance and variable hiding in C#. 
    A child class hides a superclass variable using the 'new' keyword. 
    It illustrates how member variables are accessed differently depending on whether 
    the reference is of the parent or child type.

---
+++ VariableHidingDemo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class P // Superclass
+{
+    public int a = 30; // instance variable in superclass
+}
+class Q : P // Child class inherits from P
+{
+    public new int a = 50; // hides superclass variable
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Q q = new Q(); // object of child class Q
+        Console.WriteLine("Value of a using q: " + q.a); // access child variable
+        P p = q; // superclass referencing to childclass
+        Console.WriteLine("Value of a using superclass reference: " + p.a); // access parent variable
+        Console.WriteLine("Value of a using subclass reference: " + q.a); // access child variable
+    }
+}
