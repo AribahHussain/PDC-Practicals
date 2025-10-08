f1936a973190d93f70c9595aa46a2ef54f004840
Areeba04 <aribahhussain08@gmail.com>
2025-10-08 21:51:47 +0500

    Add files via upload
    This program simulates a simple Pac-Man animation in the console using C#. 
    It demonstrates string manipulation, cursor control with carriage return (\r), and timing using Thread.Sleep(). 
    As the loop runs, Pac-Man "eats" each dot one by one, creating a smooth animated effect on the console.

---
+++ PacManAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Xml.Linq;
+class Program
+{
+    static void Main()
+    {
+        string line = “------------"; // dots Pac-Man will eat
+    int length = line.Length;
+        for (int i = 0; i < length; i++)
+
+        { // Pac-Man eats the dot -> blink effect
+            Console.Write("\r" + new string(' ', i) + "C" + line.Substring(i + 1));
+            Thread.Sleep(200); // Pac-Man arrives
+            Console.Write("\r" + new string(' ', i + 1) + line.Substring(i + 1));
+            Thread.Sleep(200); // Dot disappears (blink)
+        }
+
+        Console.Write("\r" + new string(' ', length) + "C"); // Pac-Man at end
+        Console.WriteLine("\n\nGame Over! Pac-Man ate all the dots!");
+    }
+}
